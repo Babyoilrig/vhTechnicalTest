3dc95ba84e7d97bb7197bf90fdf5a896b56f0f0a
babyoilrig <catrin.mentzoni@yahoo.com>
2023-12-09 21:35:42 +0000

    Correcting all the classes

--- js/app.js
+++ js/app.js
@@ -1,2 +1,27 @@
-//Need to try and style the spacer div in the daily does section 
-//- can't get it to show up???
+console.log("It works!");
+
+//Selecting elements
+
+const dailyDosagesButtons = document.querySelectorAll('.dailyDosages');
+console.log(dailyDosagesButtons);
+
+const administerFrequencyButtons = document.querySelectorAll(".administerFrequency");
+console.log(administerFrequencyButtons);
+
+const totalDosage = document.querySelector('.total-dosage');
+console.log(totalDosage);
+
+//This one isnt working
+const tabletTotalOneFifty = document.querySelector('.tablet-total-value-150');
+console.log(tabletTotalOneFifty);
+
+const tabletTotalSeventyFive = document.querySelector('.tablet-total-value-seventyfive');
+console.log(tabletTotalSeventyFive);
+
+
+
+
+
+
+
+
