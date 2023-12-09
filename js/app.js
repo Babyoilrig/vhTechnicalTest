ce1ae1e8846fead23cca6554fc4dfc8dc05e911e
babyoilrig <catrin.mentzoni@yahoo.com>
2023-12-09 21:49:49 +0000

    Selecting all the elements on the page

--- js/app.js
+++ js/app.js
@@ -11,12 +11,21 @@ console.log(administerFrequencyButtons);
 const totalDosage = document.querySelector('.total-dosage');
 console.log(totalDosage);
 
-//This one isnt working
 const tabletTotalOneFifty = document.querySelector('.tablet-total-value-150');
 console.log(tabletTotalOneFifty);
 
-const tabletTotalSeventyFive = document.querySelector('.tablet-total-value-seventyfive');
+const tabletTotalSeventyFive = document.querySelector('.tablet-total-value-75');
 console.log(tabletTotalSeventyFive);
+
+const tabletDailyValueOneFifty = document.querySelector('.tablet-daily-value-150');
+console.log(tabletDailyValueOneFifty);
+
+const tabletDailyValueSeventyFive = document.querySelector('.tablet-daily-value-75');
+console.log(tabletDailyValueSeventyFive);
+
+//Adding event listeners
+
+
 
 
 
