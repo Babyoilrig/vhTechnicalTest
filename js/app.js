2b73b13ccbbee7e5ecd1c4d5dfc3f01b4a8150bc
babyoilrig <catrin.mentzoni@yahoo.com>
2023-12-10 12:24:00 +0000

    Dynamically adding id to button when clicked to change colour

--- js/app.js
+++ js/app.js
@@ -25,22 +25,36 @@ console.log(tabletDailyValueSeventyFive);
 
 //Adding event listeners
 
-// Adding listeners to daily dosage buttons
+// Adding listeners to daily dosage buttons and logging value
 
 function handleDailyDoseClick(e) {
     const dailyDose = e.target.value;
+    // e.target.classList.add('clicked');
+    e.target.setAttribute('id', 'clicked');
     console.log(dailyDose);
 }
 dailyDosagesButtons.forEach(button => button.addEventListener('click', handleDailyDoseClick));
 
-
-//Adding listeners to administer frequency buttons
+//Adding listeners to administer frequency buttons and logging value
 function handleAdminFrequencyClick(e) {
     const adminFrequency = e.target.value;
     console.log(adminFrequency);
 }
 
 administerFrequencyButtons.forEach(button => button.addEventListener('click', handleAdminFrequencyClick));
+
+
+//Check two buttons clicked before running function 
+//I think you could do like: 
+// if button1.checked && button2.checked - run calculate total dosage
+
+
+
+//Function to calculate total dosage
+// function calculateTotalDosage(dailyDose, adminFrequency) {
+//     const totalDosage = dailyDose * adminFrequency;
+//     console.log(totalDosage);
+// }
 
 
 
