ccd23a42e32a874e6b29f333e132bc05e833b6ed
babyoilrig <catrin.mentzoni@yahoo.com>
2023-12-10 10:14:49 +0000

    Changing way buttons selected

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@ console.log("It works!");
 
 //Selecting elements
 
-const dailyDosagesButtons = document.querySelectorAll('.dailyDosages');
+const dailyDosagesButtons = document.querySelectorAll('.dosage-div button');
 console.log(dailyDosagesButtons);
 
-const administerFrequencyButtons = document.querySelectorAll(".administerFrequency");
+const administerFrequencyButtons = document.querySelectorAll(".administered-div button");
 console.log(administerFrequencyButtons);
 
 const totalDosage = document.querySelector('.total-dosage');
@@ -25,7 +25,16 @@ console.log(tabletDailyValueSeventyFive);
 
 //Adding event listeners
 
+//I am in the middle this - and it isnt currently 
 
+//Adding listeners to daily dosage buttons
+// dailyDosagesButtons.forEach(function(button) {
+//     button.addEventListener('click', handleDailyDoseClick);
+// });
+
+// function handleDailyDoseClick() {
+//     console.log('I have been clicked!');
+// }
 
 
 
