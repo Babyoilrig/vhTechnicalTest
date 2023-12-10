abd228a04faa66f788caf3f34a7e791509e7cb7e
babyoilrig <catrin.mentzoni@yahoo.com>
2023-12-10 21:03:25 +0000

    Commenting out console logs and also removing some unnecessary comments etc

--- js/app.js
+++ js/app.js
@@ -1,30 +1,33 @@
-console.log("It works!");
+// console.log("It works!");
+
+//NOTE - I have left all my console logs in this - (so you can see how I have worked things out). 
+//Obviously if this were production code - these wouldn't be left in.
 
 //Selecting elements
 
 const buttons = document.querySelectorAll('.button');
-console.log(buttons);
+// console.log(buttons);
 
 const dailyDosagesButtons = document.querySelectorAll('.dosage-div button');
-console.log(dailyDosagesButtons);
+// console.log(dailyDosagesButtons);
 
 const administerFrequencyButtons = document.querySelectorAll(".administered-div button");
-console.log(administerFrequencyButtons);
+// console.log(administerFrequencyButtons);
 
 const totalDosage = document.querySelector('.total-dosage');
-console.log(totalDosage);
+// console.log(totalDosage);
 
 const tabletTotalOneFifty = document.querySelector('.tablet-total-value-150');
-console.log(tabletTotalOneFifty);
+// console.log(tabletTotalOneFifty);
 
 const tabletTotalSeventyFive = document.querySelector('.tablet-total-value-75');
-console.log(tabletTotalSeventyFive);
+// console.log(tabletTotalSeventyFive);
 
 const tabletDailyValueOneFifty = document.querySelector('.tablet-daily-value-150');
-console.log(tabletDailyValueOneFifty);
+// console.log(tabletDailyValueOneFifty);
 
 const tabletDailyValueSeventyFive = document.querySelector('.tablet-daily-value-75');
-console.log(tabletDailyValueSeventyFive);
+// console.log(tabletDailyValueSeventyFive);
 
 
 // Adding listeners to daily dosage buttons and logging value
@@ -34,21 +37,21 @@ let dailyDoseInt = 0;
 function handleDailyDoseClick(e, dailyDose) {
 const blueDailyDoseButton = document.getElementById('clicked-daily-dose');
 if (blueDailyDoseButton == null) {
-    e.target.setAttribute('id', 'clicked-daily-dose');
+e.target.setAttribute('id', 'clicked-daily-dose');
 } else {
-    blueDailyDoseButton.removeAttribute('id', 'clicked-daily-dose');
-    e.target.setAttribute('id', 'clicked-daily-dose');
+blueDailyDoseButton.removeAttribute('id', 'clicked-daily-dose');
+e.target.setAttribute('id', 'clicked-daily-dose');
 }
-    dailyDoseInt = parseFloat(e.target.dataset.value);
-    console.log(dailyDoseInt);
-    // return dailyDoseInt;
+dailyDoseInt = parseFloat(e.target.dataset.value);
+console.log(dailyDoseInt);
+
 }
 dailyDosagesButtons.forEach(button => button.addEventListener('click', handleDailyDoseClick));
 
 
 
 //Adding listeners to administer frequency buttons and logging value
-//Also checking if buttons have id of clicked and removing if necessary then adding id of clicked to button clicked
+//Also checking if buttons have id of clicked and removing if necessary; then adding id of clicked to button clicked
 let adminFrequencyInt = 0;
 
 function handleAdminFrequencyClick(e, adminFrequency) {
@@ -61,31 +64,26 @@ if (blueAdminFrequencyButton == null) {
 }
     adminFrequencyInt = parseFloat(e.target.dataset.value);
     console.log(adminFrequencyInt);
-    // return adminFrequencyInt;
 }
 administerFrequencyButtons.forEach(button => button.addEventListener('click', handleAdminFrequencyClick));
 
 
-//Logic to check two buttons have been clicked before running handler function 
-
 let firstButtonClicked = false;
 let secondButtonClicked = false;
-
 let totalDosageAmount = 0;
 
-//Here (below) the dailyDoseInt and adminFrequencyInt values - aren't coming in
+//Calculating total dosage, inputting value into html
+//Calling calculate daily total function
 
 function handler() {
-    // firstButtonClicked = false;
-    // secondButtonClicked = false;
     totalDosageAmount = dailyDoseInt * adminFrequencyInt;
-    console.log('I am the handler function and I have run!');
+    // console.log('I am the handler function and I have run!');
     totalDosage.textContent = `${totalDosageAmount}mg`
     calculateDailyDosage();
     console.log(totalDosageAmount);
 }
-// are needed to be multiplied together
 
+// Logic to check two buttons have been clicked before running handler function 
 dailyDosagesButtons.forEach(button => button.addEventListener('click', ()=>{
     firstButtonClicked = true;
     if(firstButtonClicked && secondButtonClicked) handler()
@@ -98,11 +96,12 @@ administerFrequencyButtons.forEach(button => button.addEventListener('click', ()
 
 
 //Calculating daily dose
+//Calling calculate prescription total function
 let tabletDaily150 = 0;
 let tabletDaily75 = 0;
 
 function calculateDailyDosage() {
-    console.log("I am the calculateDailyDosage function and I ran!")
+    // console.log("I am the calculateDailyDosage function and I ran!");
     if(dailyDoseInt === 75){
         tabletDaily150 = 0;
         tabletDaily75 = 1;
@@ -132,7 +131,7 @@ tabletTotal150 = 0;
 tabletTotal75 = 0;
 
 function calculatePrescriptionTotal() {
-    console.log("I am the calculatePrescriptionTotal function and I ran!")
+    // console.log("I am the calculatePrescriptionTotal function and I ran!")
     tabletTotal150 = tabletDaily150 * adminFrequencyInt;
     tabletTotal75 = tabletDaily75 * adminFrequencyInt;
     tabletTotalOneFifty.textContent = `${tabletTotal150} x`;
